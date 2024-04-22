2fc4e639539279a00a2dee3a89a0889f7f965493
vbruhon <vivian.bruchon@le-campus-numerique.fr>
2024-04-22 15:42:16 +0200

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+    return (
+        <div className="mt-[3%] flex flex-col items-center gap-y-8">
+            <h1 className="text-5xl font-bold underline">Wordles !</h1>
+        </div>
+    )
+}
+
+export default App
